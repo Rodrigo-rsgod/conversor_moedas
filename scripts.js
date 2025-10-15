89f77dca4d1085f552cd7988fd3cdbb38db47276
Rodrigo-rsgod <deus.rodrigo@gmail.com>
2025-10-15 13:31:17 -0300

    Conversor de moedas

---
+++ scripts.js
@@ -0,0 +1,62 @@
+
+// Intl.NUmberFormat// 
+
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert") //valor em real//
+    const currencyValueConverted = document.querySelector(".currency-value") // outras moedas//
+
+    const dolarToday = 5.2
+    const euroToday = 6.2
+
+    if (currencySelect.value == "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+
+    if (currencySelect.value == "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+
+    }).format(inputCurrencyValue)
+
+}
+function Changecurrency() {
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
+
+
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dolar americano"
+        currencyImage.src = "./assets/dollar.png"
+
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.png"
+    }
+
+    convertValues()
+
+}
+
+currencySelect.addEventListener("change", Changecurrency)
+convertButton.addEventListener("click", convertValues)
